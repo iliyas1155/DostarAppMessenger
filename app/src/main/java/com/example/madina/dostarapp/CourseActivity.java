0483a189a6e5d65b51fb19e8d84d2057103cce55
Karina <xmoonan@gmail.com>
2018-05-13 18:06:49 +0600

    Add YouTube player to app

--- app/src/main/java/com/example/madina/dostarapp/CourseActivity.java
+++ app/src/main/java/com/example/madina/dostarapp/CourseActivity.java
@@ -1,21 +1,26 @@
 package com.example.madina.dostarapp;
 
 import android.os.Bundle;
-import android.support.v7.app.AppCompatActivity;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.example.madina.dostarapp.Items.Course;
+import com.example.madina.dostarapp.Utils.DeveloperKey;
+import com.google.android.youtube.player.YouTubeInitializationResult;
+import com.google.android.youtube.player.YouTubePlayer;
+import com.google.android.youtube.player.YouTubePlayerFragment;
 
 import static com.example.madina.dostarapp.CoursesActivity.getSelectedCourse;
 
-public class CourseActivity extends SampleActivity {
+public class CourseActivity extends SampleActivity implements YouTubePlayer.OnInitializedListener {
 
     TextView titleTextView, descTextView, textTextView;
+    YouTubePlayerFragment youtubeFragment;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_course);
-        getSupportActionBar().hide();
 
         titleTextView = findViewById(R.id.course_title);
         descTextView = findViewById(R.id.course_description);
@@ -26,5 +31,25 @@ public class CourseActivity extends SampleActivity {
         titleTextView.setText(course.name);
         descTextView.setText(course.desc);
         textTextView.setText(course.text);
+
+        initYoutubeFragment();
+    }
+
+    private void initYoutubeFragment() {
+        youtubeFragment =
+                (YouTubePlayerFragment) getFragmentManager().findFragmentById(R.id.youtube_fragment);
+        youtubeFragment.initialize(DeveloperKey.DEVELOPER_KEY, this);
+    }
+
+    @Override
+    public void onInitializationSuccess(YouTubePlayer.Provider provider, YouTubePlayer player, boolean wasRestored) {
+        if (!wasRestored) {
+            player.cueVideo("nCgQDjiotG0"); // todo: Add dynamic link.
+        }
+    }
+
+    @Override
+    public void onInitializationFailure(YouTubePlayer.Provider provider, YouTubeInitializationResult youTubeInitializationResult) {
+        Toast.makeText(this, "Cannot load the video.", Toast.LENGTH_SHORT).show();
     }
 }
